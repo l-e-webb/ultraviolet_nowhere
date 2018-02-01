496263cbecafa1d3f02e730ee0c7a4921d3747c5
Louis <louis.webb5@gmail.com>
2018-01-31 18:04:39 -0800

    Add build script.

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+public class BuildScript {
+
+    private const string EXECUTABLE_NAME = "UltravioletNowhere";
+    private const string WINDOWS_PATH = "build/UltravioletNowhere_win";
+    private const string MAC_PATH = "build/UltravioletNowhere_mac";
+    private const string LINUX_PATH = "build/UltravioletNowhere_linux";
+    private const string WEB_PATH = "build/UltravioletNowhere_web";
+    //private const string GH_PAGES_DISTRO_PATH = "webGL";
+
+    [MenuItem("BuildTools/Build all")]
+    public static void BuildAll() {
+        string[] scenes = new string[] { "Assets/Scenes/MainScene.unity" };
+
+        BuildPipeline.BuildPlayer(scenes, WINDOWS_PATH + "/" + EXECUTABLE_NAME + ".exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, MAC_PATH + "/" + EXECUTABLE_NAME, BuildTarget.StandaloneOSX, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, LINUX_PATH + "/" + EXECUTABLE_NAME, BuildTarget.StandaloneLinuxUniversal, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, WEB_PATH, BuildTarget.WebGL, BuildOptions.None);
+        //FileUtil.DeleteFileOrDirectory(GH_PAGES_DISTRO_PATH + "/");
+        //FileUtil.CopyFileOrDirectory(WEB_PATH, GH_PAGES_DISTRO_PATH + "/");
+    }
+
+}
